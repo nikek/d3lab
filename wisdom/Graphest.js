f73b91b642994fcdf1fa39b9796d25ae27ad9e6b
Niklas Ek <nikek@spotify.com>
2013-12-09 04:13:01 +0100

    More Graph examples and awesome refactoring
    Change-Id: I30c6ec1f051c3900475b1c33b2528b0c8a6412cf

--- wisdom/Graphest.js
+++ wisdom/Graphest.js
@@ -4,27 +4,17 @@ var Wisdom = Wisdom || {};
 Wisdom.Graphest = (function(undefined){
 
   function Graphest(input){
-    var that = this;
+    
     // Setup stage
     this.w = 900;
     this.h = 600;
+    this.inputData = input.data;
     this.useArea = input.area || false;
     this.colorClass = input.colorClass || [];
     this.interpolator = input.interpolator || "cardinal";
     this.tension = input.tension || 0.96;
     this.stacked = input.stacked || false;
     this.transitionTime = input.transitionTime || 300;
-
-    // Prepare stacked layout.
-    this.stack = d3.layout.stack()
-      .offset("zero")
-      .values(function(d){ return d; });
-
-
-    // Setup metrics
-    this.inputData = input.data;
-    this.setupDataset();
-
 
     // Create SVG element
     this.svg = d3.select(input.wrapper)
@@ -33,10 +23,38 @@ Wisdom.Graphest = (function(undefined){
       .append("svg")
       .attr("viewBox", "0 0 "+this.w+" "+this.h);
 
-    // Setup scales for renders
-    this.setupScales();
 
-    // Renderers
+    this.setupLayouts();    
+    this.setupDataset();
+    this.setupScales();
+    this.setupRenderers();
+
+    this.render();
+  }
+
+  Graphest.prototype.setupLayouts = function(){
+    this.stack = d3.layout.stack()
+      .offset("zero")
+      .values(function(d){ return d; });
+  }
+  
+  Graphest.prototype.setupDataset = function(){
+    this.dataset = clone(this.inputData);
+
+    if(!this.stacked){
+      this.dataset.forEach(function(series){
+        series.forEach(function(point){
+          point.y0 = 0;
+        });
+      });
+    }
+    else{
+      this.dataset = this.stack(this.dataset);
+    }
+  };
+
+  Graphest.prototype.setupRenderers = function(){
+    var that = this;
     this.line = d3.svg.line()
       .x(function(d) { return that.x(d.x); })
       .y(function(d) { return that.y(d.y+d.y0); })
@@ -49,35 +67,12 @@ Wisdom.Graphest = (function(undefined){
       .y1(function(d) { return (that.y(d.y+d.y0)); })
       .interpolate(this.interpolator)
       .tension(this.tension);
-
-
-    // INIT
-    this.render();
-  }
-
-  Graphest.prototype.setupDataset = function(){
-    var that = this;
-
-    this.dataset = clone(this.inputData);
-
-    if(!this.stacked){
-      this.dataset.forEach(function(series){
-        series.forEach(function(point){
-          point.y0 = 0;
-        });
-      });
-    }
-    else{
-      this.dataset = this.stack(that.dataset);
-    }
   };
 
-  // RENDER
   Graphest.prototype.render = function(){
-    this.genLine();
-    if(this.useArea) this.genArea();
+    this.draw("line");
+    if(this.useArea) this.draw("area");
   };
-
 
   Graphest.prototype.update = function(){
     this.setupDataset();
@@ -94,50 +89,29 @@ Wisdom.Graphest = (function(undefined){
 
   // GRAPH CONTROLS
   Graphest.prototype.setupScales = function(){
-    var that = this;
-    this.x = d3.scale.linear();
-    this.y = d3.scale.linear();
-
     this.xMin = d3.min(this.dataset, function(d){ return d[0].x; });
     this.xMax = d3.min(this.dataset, function(d){ return d.length; });
-
     this.yMax = d3.max(this.dataset, function(d){ return d3.max(d, function(d){ return d.y+d.y0; }); });
 
-    this.x.domain([this.xMin, this.xMax]).range([0, this.w]);
-    this.y.domain([0, this.yMax]).range([this.h, 3]); // last num here yields a small space for cardinal overflow
+    this.x = d3.scale.linear().domain([this.xMin, this.xMax]).range([0, this.w]);
+    this.y = d3.scale.linear().domain([0, this.yMax]).range([this.h, 3]); // last num here yields a small space for cardinal overflow
   };
 
 
-  // GENERATORS
-  Graphest.prototype.genLine = function(){
+  // GENERATOR
+  Graphest.prototype.draw = function(type){
     var that = this;
-    var path = this.svg.selectAll('.line').data(this.dataset);
+    var path = this.svg.selectAll('.'+type).data(this.dataset);
 
     path.enter().append('path')
-        .attr('id', function(d, i) { return 'line'+i; })
-        .attr('class', function(d, i) { return 'line ' + that.colorClass[i]; });
-
-    path
-      .transition()
-      .duration(this.transitionTime)
-      .attr('d', this.line);
-  };
-
-
-  Graphest.prototype.genArea = function(){
-    var that = this;
-    var path = this.svg.selectAll('.area').data(this.dataset);
-
-    path.enter().append('path')
-      .attr('id', function(d, i) { return 'area'+i; })
-      .attr('class', function(d, i) { return 'area ' + that.colorClass[i]; });
+      .attr('id', function(d, i) { return type+i; })
+      .attr('class', function(d, i) { return type + ' ' + that.colorClass[i]; });
     
     path
       .transition()
       .duration(this.transitionTime)
-      .attr("d", this.area);
+      .attr("d", this[type]); // renderer function, declared in constructor
   };
-
 
 
 
