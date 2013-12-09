15186e195dbd8e3bba1546bd5c709fac002ad746
Niklas Ek <nikek@spotify.com>
2013-12-09 02:06:48 +0100

    Stacked possible, also ability to toggle. Better datagenerator function, choose amount of series and datapoints

--- wisdom/Graphest.js
+++ wisdom/Graphest.js
@@ -8,13 +8,23 @@ Wisdom.Graphest = (function(undefined){
     // Setup stage
     this.w = 900;
     this.h = 600;
-    this.useArea = input.useArea || false;
-    this.colorClass = input.colorClass || "";
+    this.useArea = input.area || false;
+    this.colorClass = input.colorClass || [];
     this.interpolator = input.interpolator || "cardinal";
     this.tension = input.tension || 0.96;
+    this.stacked = input.stacked || false;
+    this.transitionTime = input.transitionTime || 300;
+
+    // Prepare stacked layout.
+    this.stack = d3.layout.stack()
+      .offset("zero")
+      .values(function(d){ return d; });
+
 
     // Setup metrics
-    this.dataPoints = input.data;
+    this.inputData = input.data;
+    this.setupDataset();
+
 
     // Create SVG element
     this.svg = d3.select(input.wrapper)
@@ -23,71 +33,130 @@ Wisdom.Graphest = (function(undefined){
       .append("svg")
       .attr("viewBox", "0 0 "+this.w+" "+this.h);
 
+    // Setup scales for renders
     this.setupScales();
 
+    // Renderers
     this.line = d3.svg.line()
       .x(function(d) { return that.x(d.x); })
-      .y(function(d) { return that.y(d.y); })
+      .y(function(d) { return that.y(d.y+d.y0); })
       .interpolate(this.interpolator)
       .tension(this.tension);
 
     this.area = d3.svg.area()
       .x(function(d) { return that.x(d.x); })
-      .y0(function(d) { return that.y(d.y); })
-      .y1(function(d) { return that.y(-that.maxY); })
+      .y0(function(d) { return that.y(d.y0); })
+      .y1(function(d) { return (that.y(d.y+d.y0)); })
       .interpolate(this.interpolator)
       .tension(this.tension);
+
 
     // INIT
     this.render();
   }
 
+  Graphest.prototype.setupDataset = function(){
+    var that = this;
+
+    this.dataset = clone(this.inputData);
+
+    if(!this.stacked){
+      this.dataset.forEach(function(series){
+        series.forEach(function(point){
+          point.y0 = 0;
+        });
+      });
+    }
+    else{
+      this.dataset = this.stack(that.dataset);
+    }
+  };
 
   // RENDER
   Graphest.prototype.render = function(){
-    this.setupScales();
     this.genLine();
     if(this.useArea) this.genArea();
   };
 
 
+  Graphest.prototype.update = function(){
+    this.setupDataset();
+    this.setupScales();
+    this.render();
+  };
+
   // DATA HANDLING
-  Graphest.prototype.addValue = function(value){
-    this.dataPoints.push(value);
+  Graphest.prototype.addValue = function(index, value){
+    this.dataset[index].push(value);
     this.render();
   };
 
 
   // GRAPH CONTROLS
   Graphest.prototype.setupScales = function(){
-    this.minX = this.dataPoints[0].x;
-    this.maxY = d3.max(this.dataPoints, function(d){ return d.y; });
-    this.x = d3.scale.linear().domain([this.minX, this.dataPoints.length-1]).range([0, this.w]);
-    this.y = d3.scale.linear().domain([0, this.maxY]).range([this.h, 3]); // last num here yields a small space for cardinal overflow
+    var that = this;
+    this.x = d3.scale.linear();
+    this.y = d3.scale.linear();
+
+    this.xMin = d3.min(this.dataset, function(d){ return d[0].x; });
+    this.xMax = d3.min(this.dataset, function(d){ return d.length; });
+
+    this.yMax = d3.max(this.dataset, function(d){ return d3.max(d, function(d){ return d.y+d.y0; }); });
+
+    this.x.domain([this.xMin, this.xMax]).range([0, this.w]);
+    this.y.domain([0, this.yMax]).range([this.h, 3]); // last num here yields a small space for cardinal overflow
   };
 
 
   // GENERATORS
   Graphest.prototype.genLine = function(){
+    var that = this;
+    var path = this.svg.selectAll('.line').data(this.dataset);
 
-    var path = this.svg.selectAll('.line')
-                  .data([this.dataPoints]);
+    path.enter().append('path')
+        .attr('id', function(d, i) { return 'line'+i; })
+        .attr('class', function(d, i) { return 'line ' + that.colorClass[i]; });
 
-    path.enter().append('path').attr('class', 'line ' + this.colorClass);
-    path.attr('d', this.line);
+    path
+      .transition()
+      .duration(this.transitionTime)
+      .attr('d', this.line);
   };
 
 
   Graphest.prototype.genArea = function(){
-    
-    var area = this.svg.selectAll('.area')
-                  .data([this.dataPoints]);
+    var that = this;
+    var path = this.svg.selectAll('.area').data(this.dataset);
 
-    area.enter().append('path').attr('class', 'area ' + this.colorClass);
-    area.attr('d', this.area);
+    path.enter().append('path')
+      .attr('id', function(d, i) { return 'area'+i; })
+      .attr('class', function(d, i) { return 'area ' + that.colorClass[i]; });
+    
+    path
+      .transition()
+      .duration(this.transitionTime)
+      .attr("d", this.area);
   };
 
 
+
+
+  // Public methods 
+  Graphest.prototype.toggleStacked = function(){
+    this.stacked = !this.stacked;
+    this.update();
+  };
+
+
+  // Helper functions
+  function clone(obj) {
+    if (typeof obj !== "object") return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+  }
 
   return Graphest;
 
